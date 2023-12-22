8c8237ed7c37c8302ebb94168ae4dbc77776c81e
Judy Alvarez <judy.andie.alvarez@icloud.com>
2023-12-22 14:59:13 +0200

    initial

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-tca-appl",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Appl",
+            targets: ["Appl"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.5.5"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Appl",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
+        .testTarget(
+            name: "ApplTests",
+            dependencies: ["Appl"]),
+    ]
+)
---
+++ Sources/swift-tca-appl/swift_tca_appl.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct Appl {
+}
+
+extension Appl {
+    public struct Dependencies {
+        
+    }
+    
+    public struct Features {
+        
+    }
+}
